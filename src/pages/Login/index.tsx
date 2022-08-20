1298a736bbb1e55588693b25c8072e301723a89c
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-08-20 15:25:03 -0300

    feature/ create page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,45 @@
-import { Heading } from "@chakra-ui/react"
+import { Flex, Grid, Heading, Image, Text } from "@chakra-ui/react";
 
-export const Login = () =>{
-    return <Heading>Login</Heading>
-}
+import LogoSecondary from "../../assets/logo-secondary.svg";
+
+export const Login = () => {
+  return (
+    <Flex
+      padding="10px 15px"
+      alignItems="center"
+      height="100vh"
+      bgGradient="linear(to-r, purple.800 65%, white 35%)"
+      color="white"
+    >
+      <Flex
+        w="100%"
+        justifyContent="center"
+        flexDirection="row"
+        alignItems="center"
+      >
+        <Grid w="100%" paddingRight="100px">
+          <Image src={LogoSecondary} alt="doit" boxSize="120px" />
+          <Heading as="h1">O jeito fácil, grátis</Heading>
+          <Text>
+            Flexível e atrativo de gerenciar
+            <b> seus projetos em uma única plataforma</b>
+          </Text>
+        </Grid>
+        <Grid
+          as="form"
+          mt="4"
+          w="100%"
+          padding="30px 15px"
+          border="3px solid"
+          borderColor="gray.100"
+          bg="white"
+          color="gray.900"
+        >
+            <Heading size="lg">
+                Bem vindo de voltaI
+            </Heading>
+        </Grid>
+      </Flex>
+    </Flex>
+  );
+};
