dbb4911485b51c9449ce8afd8d3efc8a294514f0
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-08-21 17:13:19 -0300

    feature: Creation of the stylized and responsive singup page and also already connected to api

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import { useForm } from "react-hook-form";
 
@@ -8,6 +8,8 @@ import { useState } from "react";
 import { useAuth } from "../../contexts/Auth/AuthContex";
 import { SingUpInfo } from "./SingUpInfo";
 import { SingUpForm } from "./SingUpForm";
+import { GoBackButton } from "./GoBackButton";
+import { api } from "../../services/api";
 
 const singUpSchema = yup.object().shape({
   name: yup.string().required("Nome obrigatório"),
@@ -23,6 +25,7 @@ interface SingUpData {
   email: string;
   password: string;
   name: string;
+  confirm_password: string;
 }
 
 export const SingUp = () => {
@@ -38,9 +41,22 @@ export const SingUp = () => {
     resolver: yupResolver(singUpSchema),
   });
 
-  const handleSingUp = (data: SingUpData) => {
-    console.log(data)
+  const handleSingUp = ({ name, email, password }: SingUpData) => {
+    setLoading(true);
+    api
+      .post("/register", { name, email, password })
+      .then((response) => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
   };
+
+  const iswideVersion = useBreakpointValue({
+    base: false,
+    md: true,
+  });
 
   return (
     <Flex
@@ -51,8 +67,8 @@ export const SingUp = () => {
       bgGradient={[
         "linear(to-b, purple.800 65%, white 35%)",
         "linear(to-b, purple.800 65%, white 35%)",
-        "linear(to-r, purple.800 65%, white 35%)",
-        "linear(to-r, purple.800 65%, white 35%)",
+        "linear(to-l, purple.800 65%, white 35%)",
+        "linear(to-l, purple.800 65%, white 35%)",
       ]}
       color="white"
     >
@@ -60,15 +76,30 @@ export const SingUp = () => {
         w={["100%", "100%", "90%", "65%"]}
         justifyContent="center"
         flexDirection={["column", "column", "row", "row"]}
-        alignItems="center"
       >
-        <SingUpForm
-          errors={errors}
-          handleSingUp={handleSubmit(handleSingUp)}
-          loading={loading}
-          register={register}
-        />
-        <SingUpInfo />
+        {iswideVersion ? (
+          <>
+            <GoBackButton top="75" left="25" />
+            <SingUpForm
+              errors={errors}
+              handleSingUp={handleSubmit(handleSingUp)}
+              loading={loading}
+              register={register}
+            />
+            <SingUpInfo />
+          </>
+        ) : (
+          <>
+            <GoBackButton top="10" left="75vw" />
+            <SingUpInfo />
+            <SingUpForm
+              errors={errors}
+              handleSingUp={handleSubmit(handleSingUp)}
+              loading={loading}
+              register={register}
+            />
+          </>
+        )}
       </Flex>
     </Flex>
   );
